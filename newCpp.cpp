9281ffd26bed2380cc99a0a02e2743d69d956e4e
Dzeshiko <76652787+Dzeshiko@users.noreply.github.com>
2021-12-06 10:56:31 +0200

    Оновити pxt.json, main.blocks, main.ts, newCpp.cpp

--- newCpp.cpp
+++ newCpp.cpp
@@ -1,1 +1,15 @@
+#include "pxt.h"
+#include <cstdint>
+#include <math.h>
 
+using namespace pxt;
+
+namespace cppSpace {
+    /*
+    * comment of cpp
+    */
+    //%
+    uint16_t getLux(int16_t number) {
+        return number * .976;
+    }
+}
